63a75fc4228f183b0e6272a4011092ae89923f3a
ash4rk <cool.shark.98@mail.ru>
2023-04-23 23:54:33 +0300

    FEAT: Complete 6.0 lesson

---
+++ Shaders_6_0_Blank/.#main.js
@@ -0,0 +1,1 @@
+ash4rk@ash4rk-System-Product-Name.146854:1682027710
--- Shaders_6_0_Blank/shaders/vertex-shader.glsl
+++ Shaders_6_0_Blank/shaders/vertex-shader.glsl
@@ -5,4 +5,5 @@ varying vec3 vPosition;
 
 void main() {	
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  vNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
 }
